3b8a84130ad6c4525d4b7b0ea873ca9b9b6f1f9e
BuddhiEkanayake <buddhisri12@gmail.com>
2025-03-16 13:59:58 +0530

    Make some components

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return( 
+        <div className="card">
+            <img></img>
+            <h2>Card Component</h2>
+            <p>I like to learn react master and be a good fullstack developer</p>
+        </div>
+    );
+}
+
+export default Card
