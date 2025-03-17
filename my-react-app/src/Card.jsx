dd4bfb29845f44f6882ce14848b59309ec9adfa4
BuddhiEkanayake <buddhisri12@gmail.com>
2025-03-17 22:50:00 +0530

    Changes

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,7 +1,7 @@
 function Card(){
     return( 
         <div className="card">
-            <img></img>
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbleacherreport.com%2Farticles%2F2157638-sri-lankas-kumar-sangakkara-further-cements-his-place-at-crickets-top-table&psig=AOvVaw3EWgyvdNT38N2eWdRo7N5n&ust=1742308276739000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjp4bKqkYwDFQAAAAAdAAAAABAa" alt="Test Image"></img>
             <h2>Card Component</h2>
             <p>I like to learn react master and be a good fullstack developer</p>
         </div>
